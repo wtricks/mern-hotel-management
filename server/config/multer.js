ba4ba2a49ef81867a44820966cb2dce959f0d87e
wtricks <wtricks.ak@gmail.com>
2024-10-10 14:49:12 +0530

    fixed uplaod directory

--- server/config/multer.js
+++ server/config/multer.js
@@ -2,12 +2,20 @@ import multer from 'multer'
 
 import { fileURLToPath } from 'url';
 import path from 'path'
+import fs from 'fs'
 
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const storage = multer.diskStorage({
     destination: (_req, _file, cb) => {
-        cb(null, path.join(__dirname, '..', 'uploads'));
+        const uploadPath = path.join(__dirname, '..', 'uploads');
+
+        // Check if the 'uploads' directory exists, if not, create it
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to avoid name collisions
